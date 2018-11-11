b4eab4dd2a0329ea09a1a72ab01c38372ed1c979
Trennis <trennisskruutelli@gmail.com>
2018-11-11 01:06:03 -0400

    hello

---
+++ helloWorld/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello class, let's hear something from foo")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar()
+}
+
+func foo() {
+	fmt.Println("Hello, I am in FOO")
+}
+
+func bar()  {
+	fmt.Println("and then we exited...")
+}
