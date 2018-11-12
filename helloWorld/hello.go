46e0f70469727ce42effa2cd0bc0c5e9f0c513f3
Trennis <trennisskruutelli@gmail.com>
2018-11-12 00:31:46 -0400

    kcbd

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -9,11 +9,7 @@ func main() {
 	//fmt.Println(err)
 	foo()
 	fmt.Println("something more")
-<<<<<<< HEAD
 	fmt.Println("blablabla")
-=======
-	fmt.Println("new windows line")
->>>>>>> 971de377dce5aba09ef03725cd434ade881e93ff
 	fmt.Println("Some new thing")
 
 	for i := 0; i < 100; i++ {
