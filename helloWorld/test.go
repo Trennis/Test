dc8add3391cd14b775a8bfe2dc7b73d45a0755b8
Trennis <trennisskruutelli@gmail.com>
2018-11-25 23:04:42 -0400

    Cpmmand prompt

---
+++ helloWorld/test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x = 10
+var z int
+
+func main() {
+	type T struct {
+		i    int
+		f    float64
+		next *T
+	}
+	for i := 0; i < 100; i++ {
+		if i%5 == 0 {
+			fmt.Println(i)
+
+		}
+	}
+	y := 20
+	fmt.Println("x=", x)
+	fmt.Println("y=", y)
+	fmt.Println("x+y=", x+y)
+	fmt.Println("z", z)
+	fmt.Println("z+x+y=", z+x+y)
+	var t T
+
+	t.i = 1
+	t.f = 2.5
+
+	fmt.Println(t.i, t.f)
+	fmt.Println(t)
+}
